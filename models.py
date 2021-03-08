6a2dce6b61b6a55d3b000abc5e695cf95f546991
rafaelpllopes <rafaelpllopes@gmail.com>
2021-03-08 07:37:09 -0300

    Modificando a forma de busca.

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ class ProfissionaisModel:
                 
         return profissionais
 
-    def find_profissional_by_matricula(self, matricula):
+    def find_profissional_by_matricula(self, matricula: str):
         """
             Traz profissional pela matricula
             nome e matricula
@@ -32,13 +32,13 @@ class ProfissionaisModel:
                 
         return profissional
     
-    def find_profissional_by_nome(self, nome):
+    def find_profissional_by_nome(self, nome: str):
         """
             Traz profissionais pelo nome
             nome e matricula
         """
         dados = self._db.cursor.execute(f"SELECT DISTINCT UPPER(HE02_ST_MATRICULA) as matricula, HE02_ST_NOME as nome \
-            FROM HE02 WHERE HE02_ST_NOME LIKE '{nome.upper()}%'")
+            FROM HE02 WHERE HE02_ST_NOME LIKE '%{nome.upper()}%'")
         
         resultados = dados.fetchallmap()
         
@@ -46,7 +46,7 @@ class ProfissionaisModel:
         
         for resultado in resultados:
             profissionais.append({ "nome": resultado['NOME'], "matricula": resultado['MATRICULA'] })
-        
+
         return profissionais
     
 class RegistrosModel:
@@ -54,7 +54,7 @@ class RegistrosModel:
     def __init__(self, db):
         self._db = db
     
-    def find_registros_by_matricula_and_periodo(self, matricula, mes, ano, ultimo_dia):                 
+    def find_registros_by_matricula_and_periodo(self, matricula: str, mes: str, ano: str, ultimo_dia: str):                 
         sql = f"SELECT DISTINCT HE22_DT_REGISTRO AS registro, EXTRACT(DAY FROM HE22_DT_REGISTRO) AS dia, HE01_ST_DESC AS origem FROM HE22 \
             INNER JOIN HE01 ON HE01_AT_COD = HE22_NR_EQUIP WHERE HE22_ST_MATRICULA = '{matricula.zfill(20)}' \
                 AND HE22_DT_REGISTRO BETWEEN '{ano}-{mes}-01 00:00:00' AND '{ano}-{mes}-{ultimo_dia} 23:59:59' ORDER BY HE22_DT_REGISTRO"
